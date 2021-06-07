2f61adefc3216c77cab355ea70881edb58fb2fdf
rarayukawa <r1u2u18@gmail.com>
2021-06-07 22:49:44 +0900

    deep_freezeメソッドをハッシュに対応させる

--- test/deep_freezable_test.rb
+++ test/deep_freezable_test.rb
@@ -11,4 +11,16 @@ class DeepFreezableTest < Minitest::Test
     #配列の要素が全てfreezeされているか？
     assert Team::COUNTRIES.all? { |country| country.frozen? }
   end
+
+  def test_deep_freeze_to_hash
+    #ハッシュの値は正しいか？
+    assert_equal(
+      { 'Japan' => 'yen', 'US' => 'dollar', 'India' => 'rupee' },
+      Bank::CURRENCIES
+      )
+    #ハッシュ自信がfreezeされているか？
+    assert Bank::CURRENCIES.frozen?
+    #ハッシュの要素（キーと値）が全てfreezeされているか？
+    assert Bank::CURRENCIES.all? { |key, value| key.frozen? && value.frozen? }
+  end
 end
