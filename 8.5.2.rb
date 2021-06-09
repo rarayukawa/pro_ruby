9218273b613a0266c72d0021c6c54adc5386d6cb
rarayukawa <r1u2u18@gmail.com>
2021-06-09 14:37:42 +0900

    include先のメソッドを使うモジュール

---
+++ 8.5.2.rb
@@ -0,0 +1,17 @@
+module Taggable
+  def price_tag
+    #priceメソッドはinclude先で定義されているはず、という前提
+    "#{price}円"
+  end
+end
+
+class Product
+  include Taggable
+
+  def price
+    1000
+  end
+end
+
+product = Product.new
+product.price_tag #=> "1000円"
